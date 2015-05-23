9d1ef78014587090a9c57415ac6f2aab9fa33490
AlexandrDmitriev <admitriev@magecore.com>
2015-05-23 18:56:06 +0300

    CBORO-108: Update email types for all subscribers - fix namespace

--- DependencyInjection/CompilerPass/ContactExportQueryBuilderAdapterCompilerPath.php
+++ DependencyInjection/CompilerPass/ContactExportQueryBuilderAdapterCompilerPath.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace OroCRM\Bundle\DotmailerBundle\DependencyInjection\CompilerPath;
+namespace OroCRM\Bundle\DotmailerBundle\DependencyInjection\CompilerPass;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
--- OroCRMDotmailerBundle.php
+++ OroCRMDotmailerBundle.php
@@ -5,7 +5,7 @@ namespace OroCRM\Bundle\DotmailerBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-use OroCRM\Bundle\DotmailerBundle\DependencyInjection\CompilerPath\ContactExportQueryBuilderAdapterCompilerPath;
+use OroCRM\Bundle\DotmailerBundle\DependencyInjection\CompilerPass\ContactExportQueryBuilderAdapterCompilerPath;
 
 class OroCRMDotmailerBundle extends Bundle
 {
