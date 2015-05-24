b116c7f3b077dc42f699ceb7aed2a1d78dad2ed2
AlexandrDmitriev <admitriev@magecore.com>
2015-05-24 13:47:34 +0300

    CBORO-108: Update email types for all subscribers - fix review

--- Tests/Functional/Fixtures/LoadChannelData.php
+++ Tests/Functional/Fixtures/LoadChannelData.php
@@ -6,7 +6,6 @@ use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 use Oro\Bundle\UserBundle\Migrations\Data\ORM\LoadAdminUserData;
