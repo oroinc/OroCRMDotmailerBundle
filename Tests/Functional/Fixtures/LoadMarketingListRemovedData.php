e0f7738548e06b7ed12add1f1b1d044276326164
AlexandrDmitriev <admitriev@magecore.com>
2015-05-27 18:41:44 +0300

    BAP-8327: Unsubscribed from OroCRM records are not removed from Dotmailer

---
+++ Tests/Functional/Fixtures/LoadMarketingListRemovedData.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace OroCRM\Bundle\DotmailerBundle\Tests\Functional\Fixtures;
+
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use OroCRM\Bundle\MarketingListBundle\Entity\MarketingListRemovedItem;
+
+class LoadMarketingListRemovedData extends AbstractFixture implements DependentFixtureInterface
+{
+    /**
+     * @var array
+     */
+    protected $data = [
+        [
+            'marketingListItem' => 'orocrm_dotmailer.orocrm_contact.mike.case',
+            'marketingList' => 'orocrm_dotmailer.marketing_list.fifth'
+        ]
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        foreach ($this->data as $item) {
+            $entity = new MarketingListRemovedItem();
+
+            $marketingListItem = $this->getReference($item['marketingListItem']);
+
+            $entity->setEntityId($marketingListItem->getId());
+            $entity->setMarketingList($this->getReference($item['marketingList']));
+
+            $manager->persist($entity);
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDependencies()
+    {
+        return [
+            'OroCRM\Bundle\DotmailerBundle\Tests\Functional\Fixtures\LoadDotmailerContactData'
+        ];
+    }
+}
