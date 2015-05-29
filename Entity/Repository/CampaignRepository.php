15ac8717b3394ab83fdb38ff5337614ee6eede03
AlexandrDmitriev <admitriev@magecore.com>
2015-05-29 15:37:39 +0300

    BAP-8325: DM integration failed after deleting address book - fix review

--- Entity/Repository/CampaignRepository.php
+++ Entity/Repository/CampaignRepository.php
@@ -17,25 +17,14 @@ class CampaignRepository extends EntityRepository
      */
     public function getCampaignsForRemoveQB(Channel $channel, array $keepCampaigns)
     {
-        /**
-         * Array of Address Books Ids was divided into parts because of
-         * "IN" statement has Limit of size based on DB settings.
-         * For mysql "IN" statement limited by "max_allowed_packet" setting.
-         *
-         * @link https://dev.mysql.com/doc/refman/5.0/en/comparison-operators.html#function_in
-         */
-        $chunks = array_chunk($keepCampaigns, 1000);
-
         $qb = $this->createQueryBuilder('campaign');
         $qb->select('campaign.id')
             ->where('campaign.channel =:channel');
 
-        foreach ($chunks as $keepAddressBooks) {
-            $qb->andWhere(
-                $qb->expr()
-                    ->notIn('campaign.originId', $keepAddressBooks)
-            );
-        }
+        $qb->andWhere(
+            $qb->expr()
+                ->notIn('campaign.originId', $keepCampaigns)
+        );
 
         return $qb->setParameters(['channel' => $channel]);
     }
