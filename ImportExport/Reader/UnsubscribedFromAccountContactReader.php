2671d4c484ae9657762f0d93112b4ec9ef48e356
AlexandrDmitriev <admitriev@magecore.com>
2015-06-11 18:35:06 +0300

    BAP-8397: Change error message after deleting DM integration

--- ImportExport/Reader/UnsubscribedFromAccountContactReader.php
+++ ImportExport/Reader/UnsubscribedFromAccountContactReader.php
@@ -4,6 +4,7 @@ namespace OroCRM\Bundle\DotmailerBundle\ImportExport\Reader;
 
 use Oro\Bundle\IntegrationBundle\Entity\Status;
 
+use OroCRM\Bundle\DotmailerBundle\Exception\RuntimeException;
 use OroCRM\Bundle\DotmailerBundle\Provider\Connector\AbstractDotmailerConnector;
 use OroCRM\Bundle\DotmailerBundle\Provider\Connector\ContactConnector;
 use OroCRM\Bundle\DotmailerBundle\Provider\Connector\UnsubscribedContactConnector;
@@ -14,6 +15,11 @@ class UnsubscribedFromAccountContactReader extends AbstractReader
     protected function initializeReader()
     {
         $this->logger->info('Importing Unsubscribed from Account Contacts');
+
+        if (!$channel = $this->getChannel()) {
+            $channelId = $this->context->getOption('channel');
+            throw new RuntimeException("Channel $channelId not exist");
+        }
 
         /** @var DotmailerTransport $transport */
         $transport = $this->contextMediator->getInitializedTransport($this->getChannel());
