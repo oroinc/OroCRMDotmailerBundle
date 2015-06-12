5d2ef8f037492ec4a446f3f0b19c8cc800960337
AlexandrDmitriev <admitriev@magecore.com>
2015-06-12 22:14:36 +0300

    BAP-8411: Dotmailer export is failed when integration is inactive

--- Command/ContactsExportCommand.php
+++ Command/ContactsExportCommand.php
@@ -97,10 +97,19 @@ class ContactsExportCommand extends AbstractSyncCronCommand
                 throw new RuntimeException('Channel not found');
             }
         }
-        $channels = $addressBook
-            ? [ $addressBook->getChannel() ]
-            : $this->getChannels($addressBook);
+        if ($addressBook) {
+            $channels = [ $addressBook->getChannel() ];
+        } else {
+            $channels = $this->getChannels();
+        }
+
         foreach ($channels as $channel) {
+            if (!$channel->isEnabled()) {
+                $logger->info(sprintf('Integration "%s" disabled an will be skipped', $channel->getName()));
+
+                continue;
+            }
+
             /**
              * If previous export not finished we need to update export results from Dotmailer
              * If after update export results all export batches is complete,
@@ -117,14 +126,7 @@ class ContactsExportCommand extends AbstractSyncCronCommand
                 $exportManager->updateExportResults($channel);
             } else {
                 $this->removePreviousAddressBookContactsExport($channel);
-                $this->getReverseSyncProcessor()
-                    ->process(
-                        $channel,
-                        ContactConnector::TYPE,
-                        [
-                            AbstractExportReader::ADDRESS_BOOK_RESTRICTION_OPTION => $addressBook
-                        ]
-                    );
+                $this->startExport($channel, $addressBook);
                 $exportManager->updateExportResults($channel);
             }
         }
@@ -165,5 +167,21 @@ class ContactsExportCommand extends AbstractSyncCronCommand
             ->getRepository('OroIntegrationBundle:Channel')
             ->findBy(['type' => ChannelType::TYPE, 'enabled' => true]);
         return $channels;
+    }
+
+    /**
+     * @param Channel $channel
+     * @param AddressBook $addressBook
+     */
+    protected function startExport(Channel $channel, AddressBook $addressBook = null)
+    {
+        $this->getReverseSyncProcessor()
+            ->process(
+                $channel,
+                ContactConnector::TYPE,
+                [
+                    AbstractExportReader::ADDRESS_BOOK_RESTRICTION_OPTION => $addressBook
+                ]
+            );
     }
 }
