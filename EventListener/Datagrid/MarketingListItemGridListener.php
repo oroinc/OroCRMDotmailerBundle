671dd415956da6a2d09193141b9186a35c52b6e8
Ivan Shakuta <ishakuta@gmail.com>
2015-06-15 19:04:21 +0300

    BAP-8396: Do not shown Subscribe action for Dotmailer integration in Marketing List - overwrite marketing list action configuration

--- EventListener/Datagrid/MarketingListItemGridListener.php
+++ EventListener/Datagrid/MarketingListItemGridListener.php
@@ -3,11 +3,19 @@
 namespace OroCRM\Bundle\DotmailerBundle\EventListener\Datagrid;
 
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\Query\Expr\Join;
 
 use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
+use Oro\Bundle\DataGridBundle\Datagrid\DatagridInterface;
 use Oro\Bundle\DataGridBundle\Datasource\Orm\OrmDatasource;
+use Oro\Bundle\DataGridBundle\Datasource\ResultRecordInterface;
 use Oro\Bundle\DataGridBundle\Event\BuildAfter;
+use Oro\Bundle\DataGridBundle\Extension\Action\ActionExtension;
+use Oro\Bundle\DataGridBundle\Extension\Formatter\Configuration;
+use Oro\Bundle\DataGridBundle\Extension\Formatter\Property\PropertyInterface;
 
+use OroCRM\Bundle\DotmailerBundle\Entity\AddressBook;
+use OroCRM\Bundle\DotmailerBundle\Entity\Contact;
 use OroCRM\Bundle\MarketingListBundle\Entity\MarketingList;
 
 class MarketingListItemGridListener
@@ -25,6 +33,23 @@ class MarketingListItemGridListener
         $this->registry = $registry;
     }
 
+    public function getMarketingListItemPermissions(ResultRecordInterface $record, array $actions)
+    {
+        $actions     = array_keys($actions);
+        $permissions = array();
+        foreach ($actions as $action) {
+            $permissions[$action] = true;
+        }
+
+        $isSubscribed    = (bool)$record->getValue('subscribed');
+        $wasUnsubscribed = $record->getValue('addressBookSubscribedStatus') == Contact::STATUS_UNSUBSCRIBED;
+
+        $permissions['subscribe']   = !$isSubscribed && !$wasUnsubscribed;
+        $permissions['unsubscribe'] = $isSubscribed;
+
+        return $permissions;
+    }
+
     /**
      * @param BuildAfter $event
      */
@@ -32,6 +57,7 @@ class MarketingListItemGridListener
     {
         $datagrid   = $event->getDatagrid();
         $datasource = $datagrid->getDatasource();
+
         if ($datasource instanceof OrmDatasource) {
             $mlParameter = $datasource->getQueryBuilder()->getParameter('marketingListEntity');
             $marketingList = $mlParameter ? $mlParameter->getValue() : null;
@@ -42,10 +68,60 @@ class MarketingListItemGridListener
                 ) {
                     $config = $datagrid->getConfig();
                     $this->removeColumn($config, 'contactedTimes');
+
+                    // join real subscriber status
+                    $qb = $datasource->getQueryBuilder();
+                    $rootAlias = $qb->getRootAliases()[0];
+                    $rootEntity = $qb->getRootEntities()[0];
+                    $qb->leftJoin(
+                        'OroCRM\Bundle\DotmailerBundle\Entity\AddressBookContact',
+                        'dm_ab_contact',
+                        Join::WITH,
+                        'dm_ab_contact.marketingListItemId = ' . $rootAlias . '.id AND ' .
+                        'dm_ab_contact.marketingListItemClass = \'' . $rootEntity . '\''
+                    )
+                    ->leftJoin(
+                        'dm_ab_contact.addressBook',
+                        'dm_ab',
+                        Join::WITH,
+                        'IDENTITY(dm_ab_contact.addressBook) = dm_ab.id AND IDENTITY(dm_ab.marketingList) = ' .
+                        $marketingList->getId()
+                    )
+                    ->addSelect('IDENTITY(dm_ab_contact.status) as addressBookSubscribedStatus');
+
+                    $this->rewriteActionConfiguration($datagrid);
                 }
             }
         }
+    }
 
+    /**
+     * @param DatagridInterface $datagrid
+     */
+    protected function rewriteActionConfiguration(DatagridInterface $datagrid)
+    {
+        $config = $datagrid->getConfig();
+        $actionConfiguration = [$this, 'getMarketingListItemPermissions'];
+        $callable = function (ResultRecordInterface $record) use ($actionConfiguration, $config) {
+            $result = call_user_func($actionConfiguration, $record, $config->offsetGetOr('actions', []));
+
+            return is_array($result) ? $result : [];
+        };
+
+        $propertyConfig = [
+            'type'                               => 'callback',
+            'callable'                           => $callable,
+            PropertyInterface::FRONTEND_TYPE_KEY => 'array'
+        ];
+
+        $config->offsetAddToArrayByPath(
+            sprintf(
+                '[%s][%s]',
+                Configuration::PROPERTIES_KEY,
+                ActionExtension::METADATA_ACTION_CONFIGURATION_KEY
+            ),
+            $propertyConfig
+        );
     }
 
     /**
