13fecbb099d87f487f33d3854f713e469ba5c05b
Denis Voronin <dvoronin@magecore.com>
2015-06-16 17:31:46 +0300

    BAP-8399: Create migration for PostgreSQL(>= 9.2) - json_array type and Doctrine 2.5

---
+++ Migrations/Schema/v1_1/UpdateContactJsonArrayQuery.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace OroCRM\Bundle\DotmailerBundle\Migrations\Schema\v1_1;
+
+use Doctrine\DBAL\Platforms\PostgreSQL92Platform;
+
+use Psr\Log\LoggerInterface;
+
+use Oro\Bundle\MigrationBundle\Migration\ArrayLogger;
+use Oro\Bundle\MigrationBundle\Migration\ParametrizedMigrationQuery;
+
+class UpdateContactJsonArrayQuery extends ParametrizedMigrationQuery
+{
+    public function getDescription()
+    {
+        $logger = new ArrayLogger();
+        $logger->info(
+            'Convert a column with "json_array(text)" type to "json_array" type on PostgreSQL >= 9.2 and Doctrine 2.5'
+        );
+        $this->doExecute($logger, true);
+
+        return $logger->getMessages();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(LoggerInterface $logger)
+    {
+        $this->doExecute($logger);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function doExecute(LoggerInterface $logger, $dryRun = false)
+    {
+        $platform = $this->connection->getDatabasePlatform();
+        if ($platform instanceof PostgreSQL92Platform) {
+            $updateSql = 'ALTER TABLE orocrm_dm_contact '.
+                         'ALTER COLUMN merge_var_values TYPE JSON USING merge_var_values::JSON';
+
+            $this->logQuery($logger, $updateSql);
+            if (!$dryRun) {
+                $this->connection->executeUpdate($updateSql);
+            }
+        }
+    }
+}
