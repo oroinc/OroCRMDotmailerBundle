4401fed39ed727a28b4a2ee745006b3eaee1e365
Ivan Shakuta <ishakuta@gmail.com>
2015-06-30 18:00:16 +0300

    CBORO-152: Abandoned carts doesn't tracked in email campaign statistics - rename contact API interaction method to addressbok contact, as it works by addressbook - update tests - rename compiler pass  - add import of unsubscribed contacts that was not in any addressbook

--- ImportExport/Reader/UnsubscribedFromAccountContactReader.php
+++ ImportExport/Reader/UnsubscribedFromAccountContactReader.php
@@ -55,7 +55,7 @@ class UnsubscribedFromAccountContactReader extends AbstractReader
                     'connector' => ContactConnector::TYPE
                 ],
                 [
-                    'date' => 'ASC'
+                    'date' => 'DESC'
                 ]
             );
 
