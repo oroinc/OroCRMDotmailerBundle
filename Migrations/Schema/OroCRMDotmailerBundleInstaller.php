1a7db3e71472a9550a5e2c8c2d8d16fb0e63c403
AlexandrDmitriev <admitriev@magecore.com>
2015-11-12 17:02:08 +0200

    CRM-4590: Add overlap to unsubscribed contacts Iterators - fix installer

--- Migrations/Schema/OroCRMDotmailerBundleInstaller.php
+++ Migrations/Schema/OroCRMDotmailerBundleInstaller.php
@@ -4,13 +4,20 @@ namespace OroCRM\Bundle\DotmailerBundle\Migrations\Schema;
 
 use Doctrine\DBAL\Schema\Schema;
 
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
 use Oro\Bundle\MigrationBundle\Migration\Installation;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
 use OroCRM\Bundle\DotmailerBundle\Migrations\Schema\v1_0;
 
-class OroCRMDotmailerBundleInstaller implements Installation
+class OroCRMDotmailerBundleInstaller implements Installation, ExtendExtensionAwareInterface
 {
+    /**
+     * @var ExtendExtension
+     */
+    protected $extendExtension;
+
     /**
      * {@inheritdoc}
      */
@@ -27,5 +34,18 @@ class OroCRMDotmailerBundleInstaller implements Installation
     {
         $migration = new v1_0\OroCRMDotmailerBundle();
         $migration->up($schema, $queries);
+
+        $addEnumFieldsMigration = new v1_0\AddEnumFields();
+        $addEnumFieldsMigration->setExtendExtension($this->extendExtension);
+        $addEnumFieldsMigration->up($schema, $queries);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setExtendExtension(ExtendExtension $extendExtension)
+    {
+        $this->extendExtension = $extendExtension;
     }
 }
