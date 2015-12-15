7b93f4a4d16bd80b989e3511af88b72a4942096a
AlexandrDmitriev <admitriev@magecore.com>
2015-12-15 13:49:20 +0200

    CBORO-225: Dotmailer watchdog import status break export - fix migration for master

--- Migrations/Schema/v1_2/OroCRMDotmailerBundle.php
+++ Migrations/Schema/v1_2/OroCRMDotmailerBundle.php
@@ -6,6 +6,7 @@ use Doctrine\DBAL\Schema\Schema;
 
 use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
 use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
+use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
@@ -28,23 +29,32 @@ class OroCRMDotmailerBundle implements Migration
     public function up(Schema $schema, QueryBag $queries)
     {
         $table = $schema->getTable('orocrm_dm_ab_cnt_export');
-        $table->addColumn('faults_processed', 'boolean', []);
-        $table->addIndex(['faults_processed'], 'orocrm_dm_ab_cnt_exp_fault_idx', []);
+        if (!$table->hasColumn('faults_processed')) {
+            $table->addColumn('faults_processed', 'boolean', []);
+            $table->addIndex(['faults_processed'], 'orocrm_dm_ab_cnt_exp_fault_idx', []);
+        }
 
         $table = $schema->getTable('orocrm_dm_ab_contact');
-        $table->addColumn('export_id', 'string', ['notnull' => false, 'length' => 36]);
-        $table->addIndex(['export_id'], 'orocrm_dm_ab_cnt_export_id_idx', []);
+        if (!$table->hasColumn('export_id')) {
+            $table->addColumn('export_id', 'string', ['notnull' => false, 'length' => 36]);
+            $table->addIndex(['export_id'], 'orocrm_dm_ab_cnt_export_id_idx', []);
+        }
 
-        $this->extendExtension->addEnumField(
-            $schema,
-            $schema->getTable('orocrm_dm_ab_contact'),
-            'exportOperationType',
-            'dm_ab_cnt_exp_type',
-            false,
-            true,
-            [
-                'extend' => ['owner' => ExtendScope::OWNER_CUSTOM]
-            ]
+        $tableName = $this->extendExtension->getTableNameByEntityClass(
+            ExtendHelper::buildEnumValueClassName('dm_ab_cnt_exp_type')
         );
+        if (!$tableName || !$schema->hasTable($tableName)) {
+            $this->extendExtension->addEnumField(
+                $schema,
+                $schema->getTable('orocrm_dm_ab_contact'),
+                'exportOperationType',
+                'dm_ab_cnt_exp_type',
+                false,
+                true,
+                [
+                    'extend' => ['owner' => ExtendScope::OWNER_CUSTOM]
+                ]
+            );
+        }
     }
 }
