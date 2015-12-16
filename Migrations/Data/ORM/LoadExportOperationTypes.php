87dc5e2e89699c873f600aa5f51ce68d1d7d1858
AlexandrDmitriev <admitriev@magecore.com>
2015-12-16 16:53:40 +0200

    CBORO-225: Dotmailer watchdog import status break export - fix enum names

--- Migrations/Data/ORM/LoadExportOperationTypes.php
+++ Migrations/Data/ORM/LoadExportOperationTypes.php
@@ -12,7 +12,7 @@ class LoadExportOperationTypes extends AbstractEnumFixture
     /** @var array */
     protected $enumData = [
         'dm_ab_cnt_exp_type' => [
-            AddressBookContact::EXPORT_NEW_CONTACT         => 'Export New Contact',
+            AddressBookContact::EXPORT_NEW_CONTACT         => 'Create New Contact',
             AddressBookContact::EXPORT_UPDATE_CONTACT      => 'Update Contact',
             AddressBookContact::EXPORT_ADD_TO_ADDRESS_BOOK => 'Add Contact to Address Book',
         ],
