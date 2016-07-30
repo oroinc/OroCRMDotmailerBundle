dee04ef6204c456487caf9826b347ce35f88a7c9
Alexandr Parkhomenko <alexandr.parhomenk@gmail.com>
2016-07-30 19:30:14 +0300

    OEE-877: Upgrade application to the Symfony 2.8
    - Updated composer.json and composer.lock
    - Isolated test that cause segment fault
    - Fixed issue with choice_type data
    - Fixed issue with changed behavior of function `getOption` that called within choice type field
    - Fixed issue with newlines in sql expressions
    - Changed repository `a2lix/translation-form-bundle` to our fork
    - Fixed Oro\Bundle\CronBundle\Tests\Functinal\Command\CronCommandTest::testCheckRunDuplicateJob
    - Moved method `getExtendedType` to the trait
    - Fixed "IntegrationTypeExtension", method `getDefaultJsEngine`, `AbstractCurrencySelectionType`, `OrganizationSelectType`, `RepeatType`, `CurrencyType`, FallbackPropertyType, AbstractShippingOptionSelectType labels, ApiForm

--- Form/Type/DotmailerTransportCheckButtonType.php
+++ Form/Type/DotmailerTransportCheckButtonType.php
@@ -14,6 +14,14 @@ class DotmailerTransportCheckButtonType extends ButtonType
      */
     public function getName()
     {
+        return $this->getBlockPrefix();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
         return self::NAME;
     }
 
--- Form/Type/IntegrationSettingsType.php
+++ Form/Type/IntegrationSettingsType.php
@@ -92,6 +92,14 @@ class IntegrationSettingsType extends AbstractType
      */
     public function getName()
     {
+        return $this->getBlockPrefix();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
         return self::NAME;
     }
 }
