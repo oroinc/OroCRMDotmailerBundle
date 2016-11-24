901b07e0d63c0600e92cee6834779ce31ba6fb4d
Eugene Sodin <esodin@ukr.net>
2016-11-24 18:52:43 +0200

    CRM-5838: Migrate integration's logic from JMSJobQueue to message queue
     - Fixing topics names

--- Async/Topics.php
+++ Async/Topics.php
@@ -3,5 +3,5 @@ namespace Oro\Bundle\DotmailerBundle\Async;
 
 class Topics
 {
-    const EXPORT_CONTACTS_STATUS_UPDATE = 'oro_dotmailer.export_contacts_status_update';
+    const EXPORT_CONTACTS_STATUS_UPDATE = 'oro.dotmailer.export_contacts_status_update';
 }
