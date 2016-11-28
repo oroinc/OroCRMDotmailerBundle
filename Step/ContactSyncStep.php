c7f808b97560df28107f1cfe88ae95a2ae021f70
Sergey Stupachenko <sstupachenko@magecore.com>
2016-11-28 13:18:55 +0000

    CRM-6567: Dotmailer integration does not work at all. Contacts are not exported

--- Step/ContactSyncStep.php
+++ Step/ContactSyncStep.php
@@ -3,40 +3,17 @@
 namespace Oro\Bundle\DotmailerBundle\Step;
 
 use Akeneo\Bundle\BatchBundle\Entity\StepExecution;
-
 use Oro\Bundle\BatchBundle\Step\ItemStep;
-use Oro\Bundle\ImportExportBundle\Context\ContextRegistry;
 
 class ContactSyncStep extends ItemStep
 {
-    const STEP_NAME             = 'oro_dotmailer_importexport_contact_sync';
-    const CONTEXT_STEP_NAME_KEY = 'stepName';
-
-    /**
-     * @var ContextRegistry
-     */
-    protected $contextRegistry;
-
     /**
      * {@inheritdoc}
      */
     public function doExecute(StepExecution $stepExecution)
     {
-        $context = $this->contextRegistry->getByStepExecution($stepExecution);
-        $context->setValue(self::CONTEXT_STEP_NAME_KEY, self::STEP_NAME);
-    
         parent::doExecute($stepExecution);
-    }
-    
-    /**
-     * @param ContextRegistry $contextRegistry
-     *
-     * @return ExportItemStep
-     */
-    public function setContextRegistry(ContextRegistry $contextRegistry = null)
-    {
-        $this->contextRegistry = $contextRegistry;
 
-        return $this;
+        $stepExecution->setReadCount(0);
     }
 }
