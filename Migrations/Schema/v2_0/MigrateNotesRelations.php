58ef7a51fd11d4861328d036082ab6dc6f1f78d2
Alexandr Dmitriev <admitriev@oroinc.com>
2016-11-29 16:21:02 +0200

    AER-87: Migrate existing Notes - fix exception in case when entity was renamed but table was not

---
+++ Migrations/Schema/v2_0/MigrateNotesRelations.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Oro\Bundle\DotmailerBundle\Migrations\Schema\v2_0;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\NoteBundle\Migration\UpdateNoteAssociationKindForRenamedEntitiesMigration;
+
+class MigrateNotesRelations extends UpdateNoteAssociationKindForRenamedEntitiesMigration
+{
+    protected $entitiesNames = [
+        'Activity',
+        'AddressBook',
+        'AddressBookContact',
+        'AddressBookContactsExport',
+        'Campaign',
+        'CampaignSummary',
+        'Contact',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getRenamedEntitiesNames(Schema $schema)
+    {
+        $oldNameSpace = 'OroCRM\Bundle\DotmailerBundle\Entity';
+        $newNameSpace = 'Oro\Bundle\DotmailerBundle\Entity';
+
+        $renamedEntityNamesMapping = [];
+        foreach ($this->entitiesNames as $entityName) {
+            $renamedEntityNamesMapping["$newNameSpace\\$entityName"] = "$oldNameSpace\\$entityName";
+        }
+
+        return $renamedEntityNamesMapping;
+    }
+}
