c3fd5fad8c8894f426212ec05136cc857b04d0b0
Bogdan <btsyganovskiy@magecore.com>
2016-12-27 14:47:08 +0200

    DOT-25: Exclude one to many relations from available fields for mapping  - Exclude one to many relations from available fields for mapping  - Add virtual relations for primary address for Contact, Lead and Magento Customer

---
+++ Provider/FieldProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Oro\Bundle\DotmailerBundle\Provider;
+
+use Doctrine\Common\Persistence\Mapping\ClassMetadata;
+
+use Oro\Bundle\EntityBundle\Provider\EntityFieldProvider;
+
+class FieldProvider extends EntityFieldProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function isIgnoredRelation(ClassMetadata $metadata, $associationName)
+    {
+        //do not allow non single associations to be used in the mapping
+        if (!$metadata->isSingleValuedAssociation($associationName)) {
+            return true;
+        }
+
+        return parent::isIgnoredRelation($metadata, $associationName);
+    }
+}
