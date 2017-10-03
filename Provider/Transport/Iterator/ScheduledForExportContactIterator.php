2e8a8bf87a7397b8336c7fb3fe55c7a6cabddc0b
Serhii Zhuravel <sergeyz@users.noreply.github.com>
2017-10-03 11:35:45 +0300

    ISDOT-287: Segment is disappear in the middle of Dotmailer sync (#13056)
    * ISDOT-287: Segment is disappear in the middle of Dotmailer sync
    - use references instead of objects if possible
    - store AddressBook ids instead of objects
    - marketing list with segment is not manual
    - StateAwareCacheProvider instead of CacheProvider to manage cached entities state

--- Provider/Transport/Iterator/ScheduledForExportContactIterator.php
+++ Provider/Transport/Iterator/ScheduledForExportContactIterator.php
@@ -3,6 +3,8 @@
 namespace Oro\Bundle\DotmailerBundle\Provider\Transport\Iterator;
 
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\UnitOfWork;
 use Oro\Bundle\DotmailerBundle\Entity\AddressBook;
 
 class ScheduledForExportContactIterator extends AbstractIterator
@@ -15,8 +17,14 @@ class ScheduledForExportContactIterator extends AbstractIterator
 
     /**
      * @var AddressBook
+     * @deprecated
      */
     protected $addressBook;
+
+    /**
+     * @var int
+     */
+    protected $addressBookId;
 
     /**
      * @param AddressBook     $addressBook
@@ -25,7 +33,7 @@ class ScheduledForExportContactIterator extends AbstractIterator
     public function __construct(AddressBook $addressBook, ManagerRegistry $registry)
     {
         $this->registry = $registry;
-        $this->addressBook = $addressBook;
+        $this->addressBookId = $addressBook->getId();
     }
 
     /**
@@ -36,9 +44,15 @@ class ScheduledForExportContactIterator extends AbstractIterator
      */
     protected function getItems($take, $skip)
     {
+        /** @var EntityManagerInterface $objectManager */
+        $objectManager = $this->registry->getManagerForClass(AddressBook::class);
+
+        /** @var AddressBook $addressBook */
+        $addressBook = $objectManager->getReference(AddressBook::class, $this->addressBookId);
+
         $contacts = $this->registry
             ->getRepository('OroDotmailerBundle:Contact')
-            ->getScheduledForExportByChannelQB($this->addressBook)
+            ->getScheduledForExportByChannelQB($addressBook)
             ->setFirstResult($skip)
             ->setMaxResults($take)
             ->getQuery()
@@ -50,7 +64,7 @@ class ScheduledForExportContactIterator extends AbstractIterator
             ->getArrayResult();
 
         foreach ($contacts as &$contact) {
-            $contact[self::ADDRESS_BOOK_KEY] = $this->addressBook->getOriginId();
+            $contact[self::ADDRESS_BOOK_KEY] = $addressBook->getOriginId();
         }
         return $contacts;
     }
