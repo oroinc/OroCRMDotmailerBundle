4c4aaa08932ab6d33e8d9f58b0e327cb19299665
Oleg Popadko <olegpopadko@users.noreply.github.com>
2018-04-24 18:50:51 +0300

    CRM-8768: Dotmailer does not work on PostgreSQL with case sensitivity setting (#17906)
    - adding comments to wrong classes

--- Migrations/Schema/v1_5_1/MakeEmailsLowercase.php
+++ Migrations/Schema/v1_5_1/MakeEmailsLowercase.php
@@ -7,6 +7,10 @@ use Doctrine\DBAL\Schema\Schema;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
+/**
+ * Please, pay attention that this class has incorrect namespace - Oro should be instead of OroCRM.
+ * It can cause fatal error if you try to inherit this class.
+ */
 class MakeEmailsLowercase implements Migration
 {
     /**
