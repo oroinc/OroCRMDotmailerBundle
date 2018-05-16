376d628451e4862db845e19fe5656cbf3e5879f3
Vitaliy Berdylo <vberdylo@oroinc.com>
2018-05-16 15:53:24 +0300

    BAP-17064: Remove Auto-registration of the command deprecation

--- DependencyInjection/OroDotmailerExtension.php
+++ DependencyInjection/OroDotmailerExtension.php
@@ -18,6 +18,7 @@ class OroDotmailerExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('commands.yml');
         $container->prependExtensionConfig($this->getAlias(), array_intersect_key($config, array_flip(['settings'])));
     }
 }
