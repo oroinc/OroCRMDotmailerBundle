de072075542f667a59efb5218afec3d777731c54
Andrey Yatsenco <ayatsenko@oroinc.com>
2018-07-13 11:52:03 +0300

    BAP-17202: Impossible to update from 2.6 with demo data to master (#18876)
    - updated type of JSON columns for MYSQL
    - set missing comment for JSON columns at pgsql

---
+++ Migrations/Schema/v1_7/OroDotmailerBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Oro\Bundle\DotmailerBundle\Migrations\Schema\v1_7;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class OroDotmailerBundle implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $queries->addPostQuery(new UpdateJsonArrayQuery());
+    }
+}
