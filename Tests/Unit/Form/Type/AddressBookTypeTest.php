0839ec594a7bc3a6bac3310f1cfd69598735597d
Dmitry Khrysev <x86demon@users.noreply.github.com>
2020-03-03 17:45:05 +0200

    BB-17204: DotMailer integration does not work properly during marketing lists synchronization (#27039)

--- Tests/Unit/Form/Type/AddressBookTypeTest.php
+++ Tests/Unit/Form/Type/AddressBookTypeTest.php
@@ -5,87 +5,104 @@ namespace Oro\Bundle\DotmailerBundle\Tests\Unit\Form\Type;
 use Oro\Bundle\DotmailerBundle\Entity\AddressBook;
 use Oro\Bundle\DotmailerBundle\Form\Type\AddressBookType;
 use Oro\Bundle\DotmailerBundle\Form\Type\IntegrationSelectType;
+use Oro\Bundle\EntityConfigBundle\Provider\ConfigProvider;
 use Oro\Bundle\EntityExtendBundle\Form\Type\EnumSelectType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints as Assert;
+use Oro\Bundle\EntityExtendBundle\Tests\Unit\Form\Extension\Stub\DynamicFieldsExtensionStub;
+use Oro\Bundle\FormBundle\Form\Extension\TooltipFormExtension;
+use Oro\Bundle\IntegrationBundle\Entity\Channel;
+use Oro\Bundle\TranslationBundle\Translation\Translator;
+use Oro\Component\Testing\Unit\Entity\Stub\StubEnumValue;
+use Oro\Component\Testing\Unit\EntityTrait;
+use Oro\Component\Testing\Unit\Form\Extension\Stub\FormTypeValidatorExtensionStub;
+use Oro\Component\Testing\Unit\Form\Type\Stub\EntityType;
+use Oro\Component\Testing\Unit\Form\Type\Stub\EnumSelectType as EnumSelectTypeStub;
+use Oro\Component\Testing\Unit\FormIntegrationTestCase;
+use Oro\Component\Testing\Unit\PreloadedExtension;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 
-class AddressBookTypeTest extends \PHPUnit\Framework\TestCase
+class AddressBookTypeTest extends FormIntegrationTestCase
 {
-    /**
-     * @var AddressBookType
-     */
-    protected $type;
+    use EntityTrait;
 
-    public function setUp()
+    /**
+     * @return array
+     */
+    protected function getExtensions()
     {
-        $this->type = new AddressBookType();
+        /** @var ConfigProvider|\PHPUnit\Framework\MockObject\MockObject $configProvider */
+        $configProvider = $this->createMock(ConfigProvider::class);
+        /** @var Translator|\PHPUnit\Framework\MockObject\MockObject $translator */
+        $translator = $this->createMock(Translator::class);
+
+        return [
+            new PreloadedExtension(
+                [
+
+                    IntegrationSelectType::class => new EntityType(
+                        [
+                            '1' => $this->getEntity(Channel::class, ['id' => 1])
+                        ],
+                        IntegrationSelectType::NAME
+                    ),
+                    EnumSelectType::class => new EnumSelectTypeStub(
+                        [
+                            new StubEnumValue('Public', 'Public'),
+                            new StubEnumValue('Private', 'Private')
+                        ]
+                    )
+                ],
+                [
+                    AddressBookType::class => [
+                        new DynamicFieldsExtensionStub([
+                            [
+                                'visibility',
+                                EnumSelectType::class,
+                                ['label' => 'base_label']
+                            ]
+                        ])
+                    ],
+                    FormType::class => [
+                        new TooltipFormExtension($configProvider, $translator),
+                        new FormTypeValidatorExtensionStub()
+                    ]
+                ]
+            )
+        ];
     }
 
     public function testBuildForm()
     {
-        /** @var FormBuilderInterface|\PHPUnit\Framework\MockObject\MockObject $builder **/
-        $builder = $this->getMockBuilder(FormBuilderInterface::class)->getMock();
+        $form = $this->factory->create(AddressBookType::class);
+        $this->assertArraySubset(['data_class' => AddressBook::class], $form->getConfig()->getOptions());
 
-        $builder->expects($this->at(0))
-            ->method('add')
-            ->with(
-                'channel',
-                IntegrationSelectType::class,
-                [
-                    'label'    => 'oro.dotmailer.integration.label',
-                    'required' => true
-                ]
-            )
-            ->will($this->returnSelf());
+        $this->assertTrue($form->has('channel'));
+        $this->assertArraySubset(
+            [
+                'label' => 'oro.dotmailer.integration.label',
+                'required' => true
+            ],
+            $form->get('channel')->getConfig()->getOptions()
+        );
 
-        $builder->expects($this->at(1))
-            ->method('add')
-            ->with(
-                'name',
-                TextType::class,
-                [
-                    'label'    => 'oro.dotmailer.addressbook.name.label',
-                    'required' => true
-                ]
-            )
-            ->will($this->returnSelf());
+        $this->assertTrue($form->has('name'));
+        $this->assertArraySubset(
+            [
+                'label' => 'oro.dotmailer.addressbook.name.label',
+                'required' => true
+            ],
+            $form->get('name')->getConfig()->getOptions()
+        );
 
-        $builder->expects($this->at(2))
-            ->method('add')
-            ->with(
-                'visibility',
-                EnumSelectType::class,
-                [
-                    'label'           => 'oro.dotmailer.addressbook.visibility.label',
-                    'tooltip'         => 'oro.dotmailer.addressbook.visibility.tooltip',
-                    'enum_code'       => 'dm_ab_visibility',
-                    'excluded_values' => [AddressBook::VISIBILITY_NOTAVAILABLEINTHISVERSION],
-                    'required'        => true,
-                    'constraints'     => [new Assert\NotNull()]
-                ]
-            )
-            ->will($this->returnSelf());
-
-        $this->type->buildForm($builder, []);
-    }
-
-    public function testConfigureOptions()
-    {
-        /** @var OptionsResolver|\PHPUnit\Framework\MockObject\MockObject $resolver **/
-        $resolver = $this->getMockBuilder(OptionsResolver::class)->getMock();
-        $resolver->expects($this->once())
-            ->method('setDefaults')
-            ->with([
-                'data_class' => 'Oro\Bundle\DotmailerBundle\Entity\AddressBook'
-            ]);
-
-        $this->type->configureOptions($resolver);
-    }
-
-    public function testGetBlockPrefix()
-    {
-        $this->assertEquals('oro_dotmailer_address_book_form', $this->type->getBlockPrefix());
+        $this->assertTrue($form->has('visibility'));
+        $this->assertArraySubset(
+            [
+                'label' => 'oro.dotmailer.addressbook.visibility.label',
+                'tooltip' => 'oro.dotmailer.addressbook.visibility.tooltip',
+                'enum_code' => 'dm_ab_visibility',
+                'excluded_values' => [AddressBook::VISIBILITY_NOTAVAILABLEINTHISVERSION],
+                'required' => true
+            ],
+            $form->get('visibility')->getConfig()->getOptions()
+        );
     }
 }
